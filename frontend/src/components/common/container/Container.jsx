3bc4a9c049bc9c1f2889e61b7b8dd871aebe168b
Ag <agustinbutti-cs@hotmail.com>
2024-12-25 22:57:40 -0300

    #45 created main UI for home with detailed info of the app and account, and responsive

--- frontend/src/components/common/container/Container.jsx
+++
@@ -1,3 +0,0 @@
-export default function Container({ children, className }) {
-  return <div className={`mx-auto ${className}`}>{children}</div>;
-}
---
+++ frontend/src/pages/home/components/StatCard.jsx
@@ -0,0 +1,24 @@
+export function StatCard({ type = '', title, icon, subtitle, value, color, children }) {
+  return (
+    <div className="w-full sm:w-1/2 mb-5 md:w-1/4 lg:w-80 p-2 flex">
+      <div className="bg-[#232d42] w-full p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col h-full">
+        <div className="flex items-center justify-between mb-4">
+          <h3 className="text-xl font-semibold text-white">{title}</h3>
+          <span className={color}>{icon}</span>
+        </div>
+        {type === "partner" ? (
+          <div className="flex items-center gap-x-2">
+            <p className="text-white mb-3">{subtitle}</p>
+            <div className={color}>{value}</div>
+          </div>
+        ) : (
+          <>
+            <p className="text-white mb-3">{subtitle}</p>
+            <div className={color}>{value}</div>
+          </>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+}
