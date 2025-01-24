feccc30c55b0cde7ad6a43a6b784dca2f70d703e
Ag <agustinbutti-cs@hotmail.com>
2025-01-23 21:11:03 -0300

    #10 TS logout API

--- backend/middlewares/logoutMiddleware.ts
+++ backend/middlewares/logoutMiddleware.ts
@@ -1,8 +1,10 @@
+import { Response, Request, NextFunction} from "express";
 import promisePool from "../db/database";
+import { RowDataPacket } from "mysql2/promise";
 
 //check if the user has a refresh token in the database
-const hasTokenInDB = async (userId) => {
-  const [results] = await promisePool.query(
+const hasTokenInDB = async (userId: number): Promise<RowDataPacket | null> => {
+  const [results] = await promisePool.execute<RowDataPacket[]>(
     "SELECT user_id FROM refresh_tokens WHERE (user_id = ?)",
     [userId]
   );
@@ -10,21 +12,29 @@ const hasTokenInDB = async (userId) => {
   return results.length === 0 ? null : results[0];
 };
 
-const hasSessionOpen = async (req, res, next) => {
+interface CustomRequest extends Request {
+  userData?: {
+    id: number;
+    hasRefreshCookie: boolean;
+  };
+}
+
+const hasSessionOpen = async (req: CustomRequest, res: Response, next: NextFunction): Promise<void> => {
   try {
     let hasRefreshCookie;
     const { id } = req.body;
-    console.log(req.body)
     if (!id) {
       console.log("No id in the logout request");
-      return res.status(401).json({ message: "No user id for logout request" });
+      res.status(401).json({ message: "No user id for logout request" });
+      return;
     }
 
     const refreshDB = await hasTokenInDB(id);
 
     if (!refreshDB) {
       console.log("user has no refresh token in the db");
-      return res.status(401).json({ message: "User has no refresh token in the db" }); 
+      res.status(401).json({ message: "User has no refresh token in the db" });
+      return;
     }
     
     const refreshToken = req.cookies["refreshToken"];
@@ -32,15 +42,18 @@ const hasSessionOpen = async (req, res, next) => {
     if (!refreshToken) {
       console.log("No refresh token in cookies (logout middleware)");
       hasRefreshCookie = false;
-      return res.status(401).json({ message: "User has no refresh token in cookies" })
+      res.status(401).json({ message: "User has no refresh token in cookies" });
+      return;
+    } else {
+      hasRefreshCookie = true;
     }
-
+    
     req.userData = { id, hasRefreshCookie };
-
     next();
   } catch (error) {
     console.log("error in logout middleware ", error);
-    return res.status(500).json({ message: "Error trying to logout" });
+    res.status(500).json({ message: "Error trying to logout" });
+    return;
   }
 };
 
--- backend/routes/logout.ts
+++ backend/routes/logout.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import logoutMiddleware from '../middlewares/logoutMiddleware'
-import logoutController from '../controllers/logoutController'
+import express, { RequestHandler } from "express";
+import logoutMiddleware from "../middlewares/logoutMiddleware";
+import logoutController from "../controllers/logoutController";
 
 const router = express.Router();
 
-router.post('/', logoutMiddleware, logoutController);
+router.post("/", logoutMiddleware as RequestHandler, logoutController as RequestHandler);
 
 export default router;
