6c13a8b4ce25fb3e26202253f7ea7c551c8f0c0b
Ag <agustinbutti-cs@hotmail.com>
2025-05-12 21:29:49 -0300

    #173 Creation of entities for user profile & files

---
+++ backend/src/domain/entities/UserFile.ts
@@ -0,0 +1,8 @@
+export class UserFile {
+  constructor(
+    public readonly id: number,
+    public readonly sectionId: number,
+    public readonly publicId: string,
+    public readonly url: string
+  ) {}
+}
